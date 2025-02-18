a2801e00c571222dcede7cb4475476012d73695f
Anthony Johnson <johns9au@mail.uc.edu>
2025-02-18 14:29:30 -0500

    phase 1

--- code (start)/main.py
+++ code (start)/main.py
@@ -1,10 +1,23 @@
 from settings import *
+from pytmx.util_pygame import load_pygame
+from os.path import join
 
 class Game:
     def __init__(self):
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption('Beast Battler')
+
+        self.import_assets()
+        self.setup(self.tmx_maps['world'], 'house')
+
+    def import_assets(self):
+        self.tmx_maps = {'world': load_pygame(join('data', 'maps', 'world.tmx'))}
+        print(self.tmx_maps)
+
+    def setup(self, tmx_map, player_start_pos):
+        for x, y, surf in tmx_map.get_layer_by_name('Terrain').tiles():
+            print(x, y, surf)
 
     def run(self):
         while True:
