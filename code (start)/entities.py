b2fc87e338adb12280d4f64e7f88d3e98c7dfa44
Anthony Johnson <johns9au@mail.uc.edu>
2025-03-05 14:25:28 -0500

    map now loading

--- code (start)/entities.py
+++ code (start)/entities.py
@@ -1,16 +1,17 @@
 from settings import *
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, surf, groups):
+    def __init__(self, pos, groups):
         super().__init__(groups)
-        self.image = surf
-        self.rect = self.image.get_frect(topleft = pos)#floating point rect
+        self.image = pygame.Surface((100,100))
+        self.image.fill('red')
+        self.rect = self.image.get_frect(center = pos)#floating point rect
 
         self.direction = vector()
     
     def input(self):
         keys = pygame.key.get_pressed()
-        input_vector = vector(0, 0)
+        input_vector = vector()
         if keys[pygame.K_UP]:
             input_vector.y -= 1
         if keys[pygame.K_DOWN]:
@@ -19,11 +20,11 @@ class Player(pygame.sprite.Sprite):
             input_vector.x -= 1
         if keys[pygame.K_RIGHT]:
             input_vector.x += 1
-        
+        self.direction = input_vector
 
     def move(self, dt):
         self.rect.center += self.direction * 250 * dt
 
     def update(self, dt): #dt = delta time done for framerate
         self.input()
-        self.move() 
+        self.move(dt) 
--- code (start)/main.py
+++ code (start)/main.py
@@ -2,9 +2,11 @@ from settings import *
 from pytmx.util_pygame import load_pygame
 from os.path import join
 
-from sprites import Sprite
+from sprites import Sprite, AnimatedSprite
 from entities import Player
 from groups import AllSprites
+
+from support import *
 
 class Game:
     def __init__(self):
@@ -20,17 +22,43 @@ class Game:
         self.setup(self.tmx_maps['world'], 'house')
 
     def import_assets(self):
-        self.tmx_maps = {'world': load_pygame(join('data', 'maps', 'world.tmx'))}
-        print(self.tmx_maps)
+        self.tmx_maps = {
+            'world': load_pygame(join('data', 'maps', 'world.tmx')),
+            'hospital': load_pygame(join('data', 'maps', 'hospital.tmx'))
+            }
+
+        self.overworld_frames = {
+            'water': import_folder('graphics', 'tilesets', 'water'),
+            'coast': coast_importer(24, 12, 'graphics', 'tilesets', 'coast')
+        }
 
     def setup(self, tmx_map, player_start_pos):
-        for x, y, surf in tmx_map.get_layer_by_name('Terrain').tiles():
-            Sprite(( x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites)
+        
+        #terrain
+        for layer in ['Terrain', 'Terrain Top']:
+            for x, y, surf in tmx_map.get_layer_by_name(layer).tiles():
+                Sprite(( x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites)
 
+        #objects
+        for obj in tmx_map.get_layer_by_name('Objects'):
+            Sprite((obj.x, obj.y), obj.image, self.all_sprites)
+
+        #entities
         for obj in tmx_map.get_layer_by_name('Entities'):
-            if obj.name == 'player' and obj.properties['pos'] == player_start_pos:
-                print((obj.x, obj.y), self.all_sprites)
+            if obj.name == 'Player' and obj.properties['pos'] == player_start_pos:
+                self.player = Player((obj.x, obj.y), self.all_sprites)
             
+        #water
+        for obj in tmx_map.get_layer_by_name('Water'):
+            for x in range(int(obj.x), int(obj.x + obj.width), TILE_SIZE):
+                for y in range(int(obj.y), int(obj.y + obj.height), TILE_SIZE):
+                    AnimatedSprite((x, y), self.overworld_frames['water'], self.all_sprites)
+
+        #coast
+        for obj in tmx_map.get_layer_by_name('Coast'):
+            terrain = obj.properties['terrain']
+            side = obj.properties['side']
+            AnimatedSprite((obj.x, obj.y), self.overworld_frames['coast'][terrain][side], self.all_sprites)
 
     def run(self):
         while True:
@@ -42,8 +70,9 @@ class Game:
                     exit()
 
             #game logic
-            self.all_sprites.update()
-            self.all_sprites.draw(self.display_surface)
+            self.all_sprites.update(dt)
+            self.display_surface.fill('black')
+            self.all_sprites.draw(self.player.rect.center)
             pygame.display.update()
 
 if __name__ == '__main__':
